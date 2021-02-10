34554c64be17a0c067ee43483898b0ada46b81a9
Sanchu Varkey <sv620s@us.att.com>
2021-02-10 16:22:47 +0000

    Add new commands

--- .history/GenerateSetterAndGetter_20210210114525.sh
+++ .history/GenerateSetterAndGetter_20210210114525.sh
@@ -22,8 +22,8 @@ do
       GET_METHOD="public ""${BASH_REMATCH[1]}"" get""${BASH_REMATCH[2]^^}""${BASH_REMATCH[3]}""()";
       SET_METHOD="public void set""${BASH_REMATCH[2]^^}""${BASH_REMATCH[3]}""(""${BASH_REMATCH[1]} ${BASH_REMATCH[2]}""${BASH_REMATCH[3]}";
       
-      GET_METHOD_FOUND=$(grep -F -c "$GET_METHOD" "$File_NAME")
-      if [ "$GET_METHOD_FOUND" -eq 0 ]; then
+      GET_METHOD_FOUND=$(g -c "$GET_METHOD" "$File_NAME")
+      if [ $GET_METHOD_FOUND -eq 0 ]; then
         LIST_METHODS+=("    public ""${BASH_REMATCH[1]}"" get""${BASH_REMATCH[2]^^}""${BASH_REMATCH[3]}""(){return ""${BASH_REMATCH[2]}""${BASH_REMATCH[3]};""}")
       fi
       
--- .history/GenerateSetterAndGetter_20210210114527.sh
+++ .history/GenerateSetterAndGetter_20210210114527.sh
@@ -23,7 +23,7 @@ do
       SET_METHOD="public void set""${BASH_REMATCH[2]^^}""${BASH_REMATCH[3]}""(""${BASH_REMATCH[1]} ${BASH_REMATCH[2]}""${BASH_REMATCH[3]}";
       
       GET_METHOD_FOUND=$(grep -F -c "$GET_METHOD" "$File_NAME")
-      if [ "$GET_METHOD_FOUND" -eq 0 ]; then
+      if [ $GET_METHOD_FOUND -eq 0 ]; then
         LIST_METHODS+=("    public ""${BASH_REMATCH[1]}"" get""${BASH_REMATCH[2]^^}""${BASH_REMATCH[3]}""(){return ""${BASH_REMATCH[2]}""${BASH_REMATCH[3]};""}")
       fi
       
